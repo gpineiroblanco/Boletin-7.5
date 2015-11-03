8ad8dd8e30a16d5c6e4f10eda6778be98f9cf1b3
Toshiba <Toshiba@192.168.0.18>
2015-11-03 01:02:09 +0100

    Condicion hecha.

---
+++ src/boletin7/pkg5/Boletin75.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin7.pkg5;
+
+/**
+ *
+ * @author Toshiba
+ */
+public class Boletin75 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Condicional par1=new Condicional();
+        double num1=par1.numeros();
+        double num2=par1.numeros();
+        double num3=par1.numeros();
+        par1.comparar(num1, num2, num3);
+    }
+    
+}
